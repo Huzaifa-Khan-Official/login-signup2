42a002b37e5bc637b6d6bab7a45cc7aaef66f0a6
Huzaifa Khan <122217933+Huzaifa-Khan-Official@users.noreply.github.com>
2023-09-22 09:46:13 +0500

    done

--- login/login.js
+++ login/login.js
@@ -12,14 +12,35 @@ if (users !== null) {
 function login() {
     var email = document.querySelector("#lemail").value;
     var password = document.querySelector("#lpass").value;
-    var filterUser = allUsers.filter(function(data) {
-        return data.email == email && data.password == password && data.name
-    });
-    if (filterUser.length) {
-        localStorage.setItem("currentUser", JSON.stringify(filterUser));
-        location.href = "../index.html"
+    var errorPara = document.querySelector("#errorPara");
+
+    if (email == "" && password == "") {
+        errorPara.innerHTML = "Please fill all the inputs!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
+    } else if (email == "") {
+        errorPara.innerHTML = "Please fill the email input!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
+    } else if (password == "") {
+        errorPara.innerHTML = "Please fill the password input!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
     } else {
-        alert("Please signup first!");
-        location.href = "../signup.html";
+
+
+        var filterUser = allUsers.filter(function (data) {
+            return data.email == email && data.password == password && data.name
+        });
+        if (filterUser.length) {
+            localStorage.setItem("currentUser", JSON.stringify(filterUser));
+            location.href = "../index.html"
+        } else {
+            alert("Please signup first!");
+            location.href = "../signup/signup.html";
+        }
     }
 }
--- signup/signup.js
+++ signup/signup.js
@@ -11,16 +11,41 @@ function signup() {
     var name = document.querySelector("#sname").value;
     var email = document.querySelector("#semail").value;
     var password = document.querySelector("#spass").value;
-    var singleData = {
-        name : name,
-        email : email,
-        password : password
-    }
+    var errorPara = document.querySelector("#errorPara");
 
-    allUsers.push(singleData);
-    localStorage.setItem("users", JSON.stringify(allUsers));
-    document.querySelector("#sname").value = "";
-    document.querySelector("#semail").value = "";
-    document.querySelector("#spass").value = "";
-    location.href = "../login/login.html";
+    if (name == "" && email == "" && password == "") {
+        errorPara.innerHTML = "Please fill all the inputs!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
+    } else if (name == "") {
+        errorPara.innerHTML = "Please fill the name input!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
+    } else if (email == "") {
+        errorPara.innerHTML = "Please fill the email input!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
+    } else if (password == "") {
+        errorPara.innerHTML = "Please fill the password input!";
+        setTimeout(() => {
+            errorPara.innerHTML = "";
+        }, 4000);
+    } else {
+
+        var singleData = {
+            name: name,
+            email: email,
+            password: password
+        }
+
+        allUsers.push(singleData);
+        localStorage.setItem("users", JSON.stringify(allUsers));
+        document.querySelector("#sname").value = "";
+        document.querySelector("#semail").value = "";
+        document.querySelector("#spass").value = "";
+        location.href = "../login/login.html";
+    }
 }
